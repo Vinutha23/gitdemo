e5f0d094d8d675c54d22962824d8c3abb1c0680a
Vinutha23 <119056807+Vinutha23@users.noreply.github.com>
2025-09-04 12:16:16 +0530

    Update Welocm.java

--- gitdemo/src/gitdemo/Welocm.java
+++ gitdemo/src/gitdemo/Welocm.java
@@ -5,6 +5,7 @@ public class Welocm {
 	public static void main(String[] args) {
 		System.out.println("Welocme to Git");
 		System.out.println("Pushing the code to github");
+		System.out.println("Pulling the code from github");
 		
 	}
 
